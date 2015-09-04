1ea8bdc248e6e081d8c98f462c9aa53e543a6290
SpringHack <springhack@live.cn>
2015-09-04 21:10:50 +0800

    Changed.

--- index.php
+++ index.php
@@ -1,13 +1,77 @@
 <?php /**
         Author: SpringHack - springhack@live.cn
-        Last modified: 2015-09-04 21:01:27
+        Last modified: 2015-09-04 21:10:38
         Filename: index.php
         Description: Created by SpringHack using vim automatically.
 **/ ?>
 <?php
-	echo 'SpringHack';
-	ob_start();
-	phpinfo();
-	file_put_contents('info.html', ob_get_contents());
-	ob_end_clean();
+	define("TOKEN", "SpringHack");
+	$wechatObj = new wechatCallbackapiTest();
+	if (isset($_GET['echostr'])) {
+		$wechatObj->valid();
+	}else{
+		$wechatObj->responseMsg();
+	}
+	
+	class wechatCallbackapiTest
+	{
+		public function valid()
+		{
+			$echoStr = $_GET["echostr"];
+			if($this->checkSignature()){
+				echo $echoStr;
+				exit;
+			}
+		}
+	
+		private function checkSignature()
+		{
+			$signature = $_GET["signature"];
+			$timestamp = $_GET["timestamp"];
+			$nonce = $_GET["nonce"];
+	
+			$token = TOKEN;
+			$tmpArr = array($token, $timestamp, $nonce);
+			sort($tmpArr);
+			$tmpStr = implode( $tmpArr );
+			$tmpStr = sha1( $tmpStr );
+	
+			if( $tmpStr == $signature ){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	
+		public function responseMsg()
+		{
+			$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	
+			if (!empty($postStr)){
+				$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+				$fromUsername = $postObj->FromUserName;
+				$toUsername = $postObj->ToUserName;
+				$keyword = trim($postObj->Content);
+				$time = time();
+				$textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+				if($keyword == "?" || $keyword == "？")
+				{
+					$msgType = "text";
+					$contentStr = date("Y-m-d H:i:s",time());
+					$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+					echo $resultStr;
+				}
+			}else{
+				echo "";
+				exit;
+			}
+		}
+	}
 ?>
